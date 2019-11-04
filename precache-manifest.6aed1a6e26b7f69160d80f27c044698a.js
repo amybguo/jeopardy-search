40aee256a15da522814698f706bc63de8ab9d802
Amy Guo <amyguo@seas.upenn.edu>
2019-11-03 22:51:45 -0500

    Updates

---
+++ precache-manifest.6aed1a6e26b7f69160d80f27c044698a.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fb9b84dfcb234b93d5f9ce1cf15ead",
+    "url": "/jeopardy-search/index.html"
+  },
+  {
+    "revision": "43b210f15b312cfb121e",
+    "url": "/jeopardy-search/static/css/2.b8dd494f.chunk.css"
+  },
+  {
+    "revision": "43b210f15b312cfb121e",
+    "url": "/jeopardy-search/static/js/2.5e950219.chunk.js"
+  },
+  {
+    "revision": "c791a56cb7454e15bddb",
+    "url": "/jeopardy-search/static/js/main.0190df6f.chunk.js"
+  },
+  {
+    "revision": "d72c9d9ec1426f8620b2",
+    "url": "/jeopardy-search/static/js/runtime-main.85ccaf55.js"
+  },
+  {
+    "revision": "161ab2ad00fa982f54cb8f16f26a0af3",
+    "url": "/jeopardy-search/static/media/raleway-latin-100.161ab2ad.woff"
+  },
+  {
+    "revision": "2d7a1238fea2c2307507a59e0855ff42",
+    "url": "/jeopardy-search/static/media/raleway-latin-100.2d7a1238.woff2"
+  },
+  {
+    "revision": "5b062a0e5a1a615b73c3d0be017d2e52",
+    "url": "/jeopardy-search/static/media/raleway-latin-100italic.5b062a0e.woff"
+  },
+  {
+    "revision": "9f282cbf5d76bdef157069c79fcf5e2e",
+    "url": "/jeopardy-search/static/media/raleway-latin-100italic.9f282cbf.woff2"
+  },
+  {
+    "revision": "138feb2449027da5ec661144f6ef2c82",
+    "url": "/jeopardy-search/static/media/raleway-latin-200.138feb24.woff"
+  },
+  {
+    "revision": "3fef5ebed6aa72326bf742645ba8a331",
+    "url": "/jeopardy-search/static/media/raleway-latin-200.3fef5ebe.woff2"
+  },
+  {
+    "revision": "33855fc8800ac209c24e93b171b90784",
+    "url": "/jeopardy-search/static/media/raleway-latin-200italic.33855fc8.woff2"
+  },
+  {
+    "revision": "cc400d950a3bacf5b5fce27fb7c0555b",
+    "url": "/jeopardy-search/static/media/raleway-latin-200italic.cc400d95.woff"
+  },
+  {
+    "revision": "4baae0f7033718fc7a7a6dbb301db7cc",
+    "url": "/jeopardy-search/static/media/raleway-latin-300.4baae0f7.woff"
+  },
+  {
+    "revision": "d724dad2e61905f488d048e51d45ae3e",
+    "url": "/jeopardy-search/static/media/raleway-latin-300.d724dad2.woff2"
+  },
+  {
+    "revision": "4f9a9bcce1b9039228b803f4fd7af4a6",
+    "url": "/jeopardy-search/static/media/raleway-latin-300italic.4f9a9bcc.woff"
+  },
+  {
+    "revision": "72abfd8df65a5440b9f9ad0fb37413bd",
+    "url": "/jeopardy-search/static/media/raleway-latin-300italic.72abfd8d.woff2"
+  },
+  {
+    "revision": "43c849ea0258ce0d23a480e840881f16",
+    "url": "/jeopardy-search/static/media/raleway-latin-400.43c849ea.woff2"
+  },
+  {
+    "revision": "60b344eb8dd676754364fc5ae4500d62",
+    "url": "/jeopardy-search/static/media/raleway-latin-400.60b344eb.woff"
+  },
+  {
+    "revision": "16b1681946ff1ae64f10059518f0f4b8",
+    "url": "/jeopardy-search/static/media/raleway-latin-400italic.16b16819.woff"
+  },
+  {
+    "revision": "9d182d98e01325f30dee06e58c385c1a",
+    "url": "/jeopardy-search/static/media/raleway-latin-400italic.9d182d98.woff2"
+  },
+  {
+    "revision": "6a9b9c422e662a18013ee064fd789213",
+    "url": "/jeopardy-search/static/media/raleway-latin-500.6a9b9c42.woff2"
+  },
+  {
+    "revision": "e9163c03fd8b6ada4fd3cf87dbc7e2ae",
+    "url": "/jeopardy-search/static/media/raleway-latin-500.e9163c03.woff"
+  },
+  {
+    "revision": "20ee2c52d2e06b53d5c90d1dafcf5026",
+    "url": "/jeopardy-search/static/media/raleway-latin-500italic.20ee2c52.woff"
+  },
+  {
+    "revision": "3ffdda0b3d57c6a3ccfc61960ac10934",
+    "url": "/jeopardy-search/static/media/raleway-latin-500italic.3ffdda0b.woff2"
+  },
+  {
+    "revision": "911d926608ce81ca8d62e74b7d09d276",
+    "url": "/jeopardy-search/static/media/raleway-latin-600.911d9266.woff2"
+  },
+  {
+    "revision": "b2b7ca0eaed5270531d7a447725203b9",
+    "url": "/jeopardy-search/static/media/raleway-latin-600.b2b7ca0e.woff"
+  },
+  {
+    "revision": "7d9764eff10e7bfd87a3de44a132eaa6",
+    "url": "/jeopardy-search/static/media/raleway-latin-600italic.7d9764ef.woff"
+  },
+  {
+    "revision": "b121e5d74dbfca5940fa38c9506581a6",
+    "url": "/jeopardy-search/static/media/raleway-latin-600italic.b121e5d7.woff2"
+  },
+  {
+    "revision": "77d77f36bed0a452984832f6b5f22e3f",
+    "url": "/jeopardy-search/static/media/raleway-latin-700.77d77f36.woff2"
+  },
+  {
+    "revision": "f252da3726243df4163d7af11448fed1",
+    "url": "/jeopardy-search/static/media/raleway-latin-700.f252da37.woff"
+  },
+  {
+    "revision": "33ecb99651d19f0230f36cd6191cbf32",
+    "url": "/jeopardy-search/static/media/raleway-latin-700italic.33ecb996.woff2"
+  },
+  {
+    "revision": "e79e0f8f3d8581470f83635fef562668",
+    "url": "/jeopardy-search/static/media/raleway-latin-700italic.e79e0f8f.woff"
+  },
+  {
+    "revision": "47f455f9e7eaacb6c1efd0456b9898f9",
+    "url": "/jeopardy-search/static/media/raleway-latin-800.47f455f9.woff"
+  },
+  {
+    "revision": "7222f1fa6b1ba32e4010040ff1c5ee7f",
+    "url": "/jeopardy-search/static/media/raleway-latin-800.7222f1fa.woff2"
+  },
+  {
+    "revision": "675a7d7a777a9a07f2b10083a2a023d2",
+    "url": "/jeopardy-search/static/media/raleway-latin-800italic.675a7d7a.woff"
+  },
+  {
+    "revision": "a45bb915945f4eca1f36a3788ebe9a2b",
+    "url": "/jeopardy-search/static/media/raleway-latin-800italic.a45bb915.woff2"
+  },
+  {
+    "revision": "d04db131ad975034bf42527e9f5ecd9c",
+    "url": "/jeopardy-search/static/media/raleway-latin-900.d04db131.woff"
+  },
+  {
+    "revision": "d64bf57c79aa2e0c95705017b2867a00",
+    "url": "/jeopardy-search/static/media/raleway-latin-900.d64bf57c.woff2"
+  },
+  {
+    "revision": "b2eea53568120d184375076e84decc61",
+    "url": "/jeopardy-search/static/media/raleway-latin-900italic.b2eea535.woff"
+  },
+  {
+    "revision": "c1c435205dc2536acd219dad5c2e9634",
+    "url": "/jeopardy-search/static/media/raleway-latin-900italic.c1c43520.woff2"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/jeopardy-search/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/jeopardy-search/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/jeopardy-search/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/jeopardy-search/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/jeopardy-search/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/jeopardy-search/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/jeopardy-search/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/jeopardy-search/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/jeopardy-search/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/jeopardy-search/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/jeopardy-search/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/jeopardy-search/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/jeopardy-search/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/jeopardy-search/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/jeopardy-search/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/jeopardy-search/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/jeopardy-search/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/jeopardy-search/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/jeopardy-search/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/jeopardy-search/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/jeopardy-search/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/jeopardy-search/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/jeopardy-search/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/jeopardy-search/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  }
+]);
